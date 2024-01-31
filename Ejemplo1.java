00ddc27185761eb35e23f18cafa445084f3faa6d
daw23-02 <daw23-02@c16x09.iescierva.net>
2024-01-31 19:21:41 +0100

    Resolver

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -4,13 +4,10 @@ public class Ejemplo1{
     System.out.println("Hola mundo");
     String nombre = "Alberto";
     System.out.println("Mi nombre es " + nombre);
-<<<<<<< HEAD
     System.out.println("La suma de 2 y 2 es: " + (2+2);
-=======
 
     for(int i = 1; i <= 10;i++){
         System.out.println("Vuelta nº " + i);
     }
->>>>>>> E47
     }
 }
